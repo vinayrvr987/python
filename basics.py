e2f077f0ed022c1749c6b0486f235c54bfb89230
Ramireddy, Vinay Reddy <VinayReddy.Ramireddy@viasat.com>
2023-08-09 15:52:24 +0530

    Adding sets lists dicts

--- basics.py
+++ basics.py
@@ -34,29 +34,3 @@ print(f"your age is {age * 12}, in months")
 form_16 = "yes"
 eligible_for_loan = age > 18 and form_16 == "yes"
 print(f"Are you eligible for loan? Ans: {eligible_for_loan}")
-
-# lists are mutable
-list_of_idiots = [["1", "Vinay"], ["2", "Kushal"]]
-print(f"List is {list_of_idiots}")
-
-#tuples are immutable
-tuple_1 = ("vinay", "pooja", "rbk", "manjula")
-print(tuple_1)
-
-#sets - Not having any duplicate values
-init_set = set() #to initialize set
-set_of_vegs = {"carrot", "beans"}
-print(set_of_vegs)
-set_of_vegs.add("potato")
-print(set_of_vegs)
-set_of_vegs.remove("carrot")
-print(set_of_vegs)
-par_veg = set_of_vegs.pop()
-print(set_of_vegs)
-dummy_num_set = {1,2,3}
-union = dummy_num_set.union(set_of_vegs) #union, intersection, differance
-print(union)
-
-print(2 in dummy_num_set)
-print(2 in set_of_vegs)
-print("v" in "vinay")
